f43a45a24ebf8a03fdcfbfdae090edfed1fa70ab
CommonDrum <52042159+CommonDrum@users.noreply.github.com>
2022-09-19 22:00:43 +0100

    added core header

---
+++ src/Core/Core.h
@@ -0,0 +1,22 @@
+//
+// Created by Karol on 19/09/2022.
+//
+
+#ifndef INC_2DPHYSIX_CORE_H
+#define INC_2DPHYSIX_CORE_H
+
+#include "MyOpenGLRenderer.h"
+#include "memory"
+#include "algorithm"
+#include "utility"
+#include "functional"
+
+#include "iostream"
+#include "sstream"
+#include "string"
+#include "vector"
+
+
+
+
+#endif //INC_2DPHYSIX_CORE_H
